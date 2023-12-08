83f6353b258ccba3afc4a106ce4115693692921f
sydneydrain5 <150982313+sydneydrain5@users.noreply.github.com>
2023-12-07 19:39:09 -0500

    Add files via upload

---
+++ data/RedeemerUniversity_2.js
@@ -0,0 +1,1 @@
+var json_RedeemerUniversity_2 = {"type":"FeatureCollection","name":"RedeemerUniversity_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Name":"Redeemer"},"geometry":{"type":"Point","coordinates":[-79.950255618574602,43.210885899569703]}}]}
